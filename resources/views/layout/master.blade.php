dc708f2a4d8bc196017b5eb527280682f5428cc2
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-07-31 12:49:50 +0800

    add assets with vit engin

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>User Card - <?=$user->name?></title>
+    <title>User Card - <?=$user->name??''?></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="assets/css/main.css" />
     <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
     @stack('custom-css')
+    @vite('resources/js/app.js')
 </head>
 <body class="is-preload">
 <div id="wrapper">
     <section id="main">
-    @yield('content')
+   {{-- @yield('content')--}}
     </section>
     @include('layout.partials.footer')
 </div>
